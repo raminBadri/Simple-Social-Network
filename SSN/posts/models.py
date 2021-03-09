ab46e22e724920a88f88105b681f7de1cf09ab39
ramin <ramin.badri69@gmail.com>
2021-03-09 19:28:43 +0330

    add migration files

--- SSN/posts/models.py
+++ SSN/posts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE())
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField(max_length=500)
     slug = models.SlugField(max_length=200)
     created = models.DateTimeField(auto_now=True)
